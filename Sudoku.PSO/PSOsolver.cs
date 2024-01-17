8f4e7853b4f716d5dc5f8198c63578e72e441cfb
SofianeMob21 <mobarek.sofiane@gmail.com>
2024-01-17 17:09:24 +0100

    Ajout projet PSO

---
+++ Sudoku.PSO/PSOsolver.cs
@@ -0,0 +1,14 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.PSO
+{
+    public class PSOsolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            var tableau = s.Cells;
+            // résoudre tableau
+            return new() { Cells = tableau };
+        }
+    }
+}
